7c5fcdd4f6ac91157b104dd13db722a0fe461ff0
iliyas012345 <115693005+iliyas012345@users.noreply.github.com>
2023-01-02 16:10:19 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,38 @@
+setTimeout(function(){
+  document.getElementById('content1').innerHTML=10
+  setTimeout(function(){
+      document.getElementById('content1').innerHTML=9
+      setTimeout(function(){
+        document.getElementById('content1').innerHTML=8
+        setTimeout(function(){
+          document.getElementById('content1').innerHTML=7
+          setTimeout(function(){
+            document.getElementById('content1').innerHTML=6
+            setTimeout(function(){
+              document.getElementById('content1').innerHTML=5
+              setTimeout(function(){
+                document.getElementById('content1').innerHTML=4
+                setTimeout(function(){
+                  document.getElementById('content1').innerHTML=3
+                  setTimeout(function(){
+                    document.getElementById('content1').innerHTML=2
+                    setTimeout(function(){
+                      document.getElementById('content1').innerHTML=1
+                      setTimeout(function(){
+                        document.getElementById('content1').innerHTML='Happy Independence Day'
+                      
+      
+  
+                     }, 1000);
+                  },1000)
+                },1000);
+              },1000);
+            },1000)
+          },1000);
+        },1000);
+      },1000);
+    },1000);
+  },1000);
+},1000)
+
+
